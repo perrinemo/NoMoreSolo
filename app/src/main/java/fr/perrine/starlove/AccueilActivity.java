ba17eb2dd32bfcb584e7ea9cbc230d28023542b4
Perrine <morandperrine@hotmail.fr>
2018-05-17 21:34:02 +0200

    Initialisation de la BDD en commentaire

--- app/src/main/java/fr/perrine/starlove/AccueilActivity.java
+++ app/src/main/java/fr/perrine/starlove/AccueilActivity.java
@@ -1,17 +1,14 @@
 package fr.perrine.starlove;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -25,6 +22,7 @@ public class AccueilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
 
+        /*
         // Crée une file d'attente pour les requêtes vers l'API
         final RequestQueue requestQueue = Volley.newRequestQueue(this);
 
@@ -40,14 +38,14 @@ public class AccueilActivity extends AppCompatActivity {
 
                         try {
 
-                            for ( int i = 0; i < response.length(); i++ ) {
+                            for (int i = 0; i < response.length(); i++) {
 
                                 JSONObject characPage = response.getJSONObject(i);
 
                                 String characUserName = characPage.getString("name");
                                 String characGenre = characPage.getString("gender");
                                 String characSpecie = characPage.getString("species");
-                               // String characImg = characPage.getString("wiki");
+                                // String characImg = characPage.getString("wiki");
                                 int characMass = characPage.getInt("mass");
                                 double characHeight = characPage.getDouble("height");
 
@@ -70,6 +68,7 @@ public class AccueilActivity extends AppCompatActivity {
 
         // On ajoute la requête à la file d'attente
         requestQueue.add(JsonArrayRequest);
+        */
     }
 }
 
